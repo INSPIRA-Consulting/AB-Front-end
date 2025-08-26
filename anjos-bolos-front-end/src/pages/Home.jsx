6735b720be588a634f3da04987269e2b8596706d
JulianaFreires <juliana.silva@sptech.school>
2025-08-26 20:27:21 -0300

    pagina home

--- anjos-bolos-front-end/src/pages/Home.jsx
+++ anjos-bolos-front-end/src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import styles from '../styles/Home.module.css';
+import '../styles/fonts/fonts.css';
+
 export function Home() {
     return (
-        <div>
-            <h1 style={{ color: 'black' }}>Home Page</h1>
+        <div className={styles.homeContainer}>
+            <div className={styles.topButtons}>
+                <button className={styles.topButton}>Login</button>
+                <button className={styles.topButton}>Ajuda</button>
+            </div>
+            <div className={styles.centerContent}>
+                <h1 className={styles.title}>Boas Vindas!</h1>
+                <div className={styles.subtitle}>O bolo da anjos bolos é...</div>
+                <div className={styles.subtitle}>Uma delícia!</div>
+            </div>
         </div>
-    )
+    );
 }
