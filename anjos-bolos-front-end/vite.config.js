cc4177d72911ce89439d387a7be742c2a7f92fc0
GustavoPresilli <gustavo.spresilli@sptech.school>
2025-10-29 12:37:42 -0300

    Subindo novo CICD teste

--- anjos-bolos-front-end/vite.config.js
+++ anjos-bolos-front-end/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  // Carregar variáveis de ambiente
   const env = loadEnv(mode, process.cwd(), '')
+  const apiHost = env.VITE_IP_API || 'localhost'
   
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: `http://${env.VITE_IP_API}:8080`,
+          target: `http://${apiHost}:8080`,
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
